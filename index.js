6a0f67aa187de6012e64759cf40865de076ae03d
Erangamadhushan <erangamadhushann956@gmail.com>
2025-02-12 10:46:33 +0530

    Add viewport meta tag, update window size, and enhance styling with external CSS

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const { app, BrowserWindow } = require('electron/main');
 
 const createWindow = () => {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600
+    width: 1200,
+    height: 700
   })
 
-  win.loadFile('index.html')
+  win.loadFile('../index.html')
 }
 
 app.whenReady().then(() => {
