7b6d3b39251a29f3acd5c09e9dce0d248d91be97
Erangamadhushan <erangamadhushann956@gmail.com>
2025-02-16 03:02:08 +0530

    Implement dark mode toggle functionality and update UI; add styles for light and dark themes

--- index.js
+++ index.js
@@ -13,6 +13,19 @@ const createWindow = () => {
   win.loadFile('../index.html')
 }
 
+ipcMain.handle('dark-mode:toggle', () => {
+  if (nativeTheme.shouldUseDarkColors) {
+    nativeTheme.themeSource = 'light'
+  } else {
+    nativeTheme.themeSource = 'dark'
+  }
+  return nativeTheme.shouldUseDarkColors
+})
+
+ipcMain.handle('dark-mode:system', () => {
+  nativeTheme.themeSource = 'system'
+})
+
 app.whenReady().then(() => {
   ipcMain.handle('ping', () => 'pong')
   createWindow()
