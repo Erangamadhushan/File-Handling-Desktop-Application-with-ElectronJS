e00e3953ec17811b178707c35537efd00ce806fa
Erangamadhushan <erangamadhushann956@gmail.com>
2025-02-16 02:51:58 +0530

    Add preload script and IPC communication; update package.json for Electron Forge

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
-const { app, BrowserWindow } = require('electron/main');
+const { app, BrowserWindow, ipcMain } = require('electron/main')
+const path = require('node:path')
 
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 1200,
-    height: 700
+    height: 700,
+    webPreferences: {
+      preload: path.join(__dirname, './preload.js')
+    }
   })
 
   win.loadFile('../index.html')
 }
 
 app.whenReady().then(() => {
+  ipcMain.handle('ping', () => 'pong')
   createWindow()
 
   app.on('activate', () => {
